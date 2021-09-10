4a8fac320673d96aa5f9975b96c0a454c0f248ab
phan-le-phu <18521247@gm.uit.edu.vn>
2021-09-10 10:57:15 +0700

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'floating-stream-78680.herokuapp.com'
+]
 
 
 # Application definition
